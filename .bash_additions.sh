e3b156cbe3af1f490531830bafa280e9042e42bd
Ethan Soergel <esoergel@gmail.com>
2014-06-26 16:36:15 -0400

    minor changes

--- .bash_additions.sh
+++ .bash_additions.sh
@@ -14,7 +14,7 @@ function pull-latest-master() {
     git submodule foreach --recursive 'git checkout master; git pull origin master &'
     until [ -z "$(ps aux | grep '[g]it pull')" ]; do sleep 1; done
 }
-function code-update() {
+function update-code() {
     pull-latest-master
     git submodule update --recursive
     pyc-purge
