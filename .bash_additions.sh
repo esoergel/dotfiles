66cd99fd0b34b3d7df12080fc4fb4b9a14b443c6
Ethan Soergel <esoergel@gmail.com>
2015-04-17 20:05:08 +0200

    Update bash functions

--- .bash_additions.sh
+++ .bash_additions.sh
@@ -4,19 +4,29 @@ export PROJECT_HOME=$HOME/web_dev
 alias setvenv='source $WORKON_HOME/setvenv'
 # source /usr/local/bin/virtualenvwrapper.sh
 
+function apt-update() {
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get dist-upgrade -y
+    sudo apt-get autoremove -y
+}
+
 # git code updates
 function pyc-purge() {
     find . -name '*.pyc' -delete
 }
-function pull-latest-master() {
-    git checkout master; git pull origin master
+function pull-latest-masters() {
+    git checkout master
+    git pull origin master
     git submodule update --init
     git submodule foreach --recursive 'git checkout master; git pull origin master &'
     until [ -z "$(ps aux | grep '[g]it pull')" ]; do sleep 1; done
+    pyc-purge
 }
 function update-code() {
-    pull-latest-master
-    git submodule update --recursive
+    git checkout master
+    git pull origin master
+    git submodule update --init --recursive
     pyc-purge
 }
 
