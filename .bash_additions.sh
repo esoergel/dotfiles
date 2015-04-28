4b9ff480dc483e203b1e420680fe15778010b07a
Ethan Soergel <esoergel@gmail.com>
2015-04-28 18:57:01 +0000

    Add show-branches stuff

--- .bash_additions.sh
+++ .bash_additions.sh
@@ -10,6 +10,36 @@ function apt-update() {
     sudo apt-get dist-upgrade -y
     sudo apt-get autoremove -y
 }
+function show-branches-long() {
+    for BRANCH in `git br | grep -v '\\*'`
+    do
+        echo "$(tput rev)$BRANCH$(tput sgr0)"\
+        $(git log $BRANCH -1\
+        --pretty="
+            %C(yellow)%s
+            <break>
+            %C(magenta)%ad
+            %C(blue) [%cn]
+            %C(green)%h
+            %C(red)%d
+            %C(reset)
+            <break>
+        " --source\
+        ) | sed 's/<break>/\n/g'
+    done
+}
+function show-branches() {
+    for BRANCH in `git br | grep -v '\\*'`
+    do
+        echo $(git log $BRANCH -1\
+        --pretty="
+            %C(magenta)%ad
+            %C(reset)<name>
+            %C(yellow)%s
+            %C(reset)
+        ") | sed "s/<name>/$BRANCH -/g"
+    done
+}
 
 # git code updates
 function pyc-purge() {
